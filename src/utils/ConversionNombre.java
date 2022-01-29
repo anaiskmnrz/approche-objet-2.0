645f42fac0f979c2189d2d9e20d16092b6ac21b3
Anaïs Kominiarz <kominiarz.anais@gmail.com>
2022-01-29 15:59:51 +0100

    methodes statiques

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class ConversionNombre {
+	
+	public static void main(String[] args) {
+	
+	String chaine = "12";
+	
+
+	int nombre = Integer.parseInt(chaine);
+	
+	System.out.println(nombre);
+	
+	int a = 15;
+	int b = 25;
+	
+	int max = Math.max(a,b);
+	System.out.println(max);
+	
+	}
+}
