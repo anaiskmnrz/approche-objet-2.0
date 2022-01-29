8c415e6b5fe0425b59b694e945a86cbb030c824f
Anaïs Kominiarz <kominiarz.anais@gmail.com>
2022-01-29 16:16:48 +0100

    encapsulation

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	public int numero;
+	public int solde;
+	
+	public Compte(int numero, int solde) {
+		numero = this.numero;
+		solde = this.solde;
+	}
+
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		
+		Compte compte = new Compte(105879,250);
+		
+		// affichage incompréhensible
+		System.out.println(compte);
+		
+	}
+
+}
